9a02f00816f465378d8042f06fac1fd117e5675a
Anunnaki20 <masonOlchoway2000@gmail.com>
2021-11-26 17:08:55 -0600

    Added login and logout, added links in navbar

--- PartD/home/urls.py
+++ PartD/home/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('doughnuts/', views.doughnuts, name='doughnuts'),
+    path('about/', views.about, name='about'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutCustomer, name='logout'),
 ]
 
 
--- PartD/home/views.py
+++ PartD/home/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.decorators import login_required
 
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+@login_required(login_url="login")
+def doughnuts(request):
+    return render(request, 'doughnuts.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+
+        # Get the login form data
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Check to make sure that the user is in the database
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        # authenticate the user and log them in
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'Username OR Password does not exist')
+
+
+    context = {}
+    return render(request, 'signup_login.html', context)
+
+
+def logoutCustomer(request):
+    logout(request)
+    return redirect('homepage')
