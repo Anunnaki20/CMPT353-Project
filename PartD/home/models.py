fab2df107e20c21fa516fe6deade2f5f648025bc
Anunnaki20 <masonOlchoway2000@gmail.com>
2021-11-26 15:34:54 -0600

    Created the first model

--- PartD/home/models.py
+++ PartD/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class staff(models.Model):
+    staff_ID = models.IntegerField(null=False, unique=True)
+    firstname = models.CharField(max_length=255, null=False)
+    lastname = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255, null=False)
+    salary = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.firstname
+
