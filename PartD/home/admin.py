3e012c2a6a23528561d15228c8b686049445a354
Anunnaki20 <masonOlchoway2000@gmail.com>
2021-11-26 16:15:37 -0600

    Added another model for the customer reports

--- PartD/home/admin.py
+++ PartD/home/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import staff
+from .models import customer_report, staff
 
 admin.site.register(staff)
+admin.site.register(customer_report)
--- PartD/home/models.py
+++ PartD/home/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class staff(models.Model):
-    staff_ID = models.IntegerField(null=False, unique=True)
     firstname = models.CharField(max_length=255, null=False)
     lastname = models.CharField(max_length=255, null=False)
     title = models.CharField(max_length=255, null=False)
     salary = models.IntegerField(null=False)
 
     def __str__(self):
-        return self.firstname
+        return self.firstname + " " + self.lastname + ", " + self.title
+
+
+class customer_report(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE) # if the User is deleted we delete all the records are deleted for that User
+    order_amount = models.IntegerField(null=False)
+    report = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return " Report: " + self.report[0:50]
 
