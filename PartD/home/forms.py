cef92852bd75837503da5980bd733de0892c1368
Yafi <tahzibislamyafi@gmail.com>
2021-11-30 22:55:35 -0600

    final version hopefully

--- PartD/home/forms.py
+++ PartD/home/forms.py
@@ -7,12 +7,12 @@ from .models import User
 
 # Custome User creation form
 class UserSignUpForm(UserCreationForm):
-    # first_name = forms.CharField(widget=TextInput(attrs={'placeholder': 'First Name', 'style': 'width: 300px;'}))
-    # last_name = forms.CharField(widget=TextInput(attrs={'placeholder': 'Last Name', 'style': 'width: 300px;'}))
-    # email = forms.EmailField(widget=EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;'}))
-    # username = forms.CharField(widget=TextInput(attrs={'placeholder': 'Username', 'style': 'width: 300px;'}))
-    # password1 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Password', 'type':'password', 'style': 'width: 300px;'}))
-    # password2 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Confirm Password', 'type':'password', 'style': 'width: 300px;'}))
+    first_name = forms.CharField(widget=TextInput(attrs={'placeholder': 'First Name', 'style': 'width: 300px;'}))
+    last_name = forms.CharField(widget=TextInput(attrs={'placeholder': 'Last Name', 'style': 'width: 300px;'}))
+    email = forms.EmailField(widget=EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;'}))
+    username = forms.CharField(widget=TextInput(attrs={'placeholder': 'Username', 'style': 'width: 300px;'}))
+    password1 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Password', 'type':'password', 'style': 'width: 300px;'}))
+    password2 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Confirm Password', 'type':'password', 'style': 'width: 300px;'}))
 
     class Meta:
         model = User
